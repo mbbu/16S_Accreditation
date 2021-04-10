261c1311fbc5b3badf61ae69a187300bb84d9d6e
Caleb Kipkurui <ckibet@icipe.org>
2021-04-10 04:29:18 +0300

    Delete Plots.R

--- Dada2_Pipeline/Plots.R
+++
@@ -1,9 +0,0 @@
-list.files()
-script.dir <- getSrcDirectory(function(x) {x})
-setwd(script.dir)
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-getwd()
-phyloseq_object <- readRDS('../Results/phyloseq_object.rds')
-
-
-
